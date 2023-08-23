049bf78939aeb5eff5650232a85882edd9381608
Sinduja B <127333575+Sindu9156@users.noreply.github.com>
2023-08-23 13:32:10 +0530

    addition.c

---
+++ addition.c
@@ -0,0 +1,11 @@
+
+#include<stdio.h>
+
+void main(){
+int a,b;
+printf("enter the first value");
+scanf("%d",&a);
+printf("enter the second value");
+scanf("%d",&b);
+printf("the addition of %d and %d is ",a,b,a+b);
+}
