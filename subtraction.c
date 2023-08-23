91c35a512622ff3e3e77c0fcf6010ad162b772e4
Sinduja B <127333575+Sindu9156@users.noreply.github.com>
2023-08-23 13:39:28 +0530

    subtraction.c

---
+++ subtraction.c
@@ -0,0 +1,11 @@
+
+#include<stdio.h>
+
+void main(){
+int a,b;
+printf("enter the first value");
+scanf("%d",&a);
+printf("enter the second value");
+scanf("%d",&b);
+printf("the subtraction of %d and %d is %d ",a,b,a-b);
+}
