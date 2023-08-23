b7e90dba7b332d0cc0df7fec89d6eb0457893f53
Sinduja B <127333575+Sindu9156@users.noreply.github.com>
2023-08-23 13:34:53 +0530

    addition.c

--- addition.c
+++ addition.c
@@ -7,5 +7,5 @@ printf("enter the first value");
 scanf("%d",&a);
 printf("enter the second value");
 scanf("%d",&b);
-printf("the addition of %d and %d is ",a,b,a+b);
+printf("the addition of %d and %d is %d ",a,b,a+b);
 }
