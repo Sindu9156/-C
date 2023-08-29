8b4b652167f11204f06b6f8f9cfacd17ae939868
Sinduja B <127333575+Sindu9156@users.noreply.github.com>
2023-08-29 23:10:13 +0530

    leapyear.c

---
+++ leapyear.c
@@ -0,0 +1,13 @@
+include<stdio.h>
+void main()
+{
+    int year;
+    printf("enter the year" );
+   
+        if(year%4==0)
+            printf("\n%d is a leap year ",year);
+        else 
+            printf("\n%d is not a leap year ",year);
+
+
+}
