74f4ae08006146a94aa7c66e918256f00e7ec5e1
Sinduja B <127333575+Sindu9156@users.noreply.github.com>
2023-09-23 18:43:17 +0530

    Area of square.c

---
+++ Area of square.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+float area,r,pi=3.14;
+printf("enter the radius");
+scanf("%f",&r);
+area=pi*r*r;
+printf("the area of circle is %f",area); 
+
+    return 0;
+}
