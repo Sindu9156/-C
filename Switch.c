b040d46973f1262fc39abd2ac739d15a05eefc96
Sinduja B <127333575+Sindu9156@users.noreply.github.com>
2023-10-19 18:38:24 +0530

    Switch.c

---
+++ Switch.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main(){
+    int i;
+    printf("\n Enter the number");
+    scanf("%d",&i);
+    printf("\n the winner is");
+    switch(i){
+        case 1:printf("\nvarun");
+               break;
+        case 2:printf("\nraju");
+               break;
+        case 3:printf("\nnirub");
+               break;
+        case 4:printf("\nciby");
+               break;
+    
+    }
+}
