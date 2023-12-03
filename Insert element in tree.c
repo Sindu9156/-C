42f83fed41c98d742c22653798b48241ffd4f9f5
Sinduja B <127333575+Sindu9156@users.noreply.github.com>
2023-12-03 19:29:07 +0530

    Insert element in tree.c

---
+++ Insert element in tree.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+typedef struct tree *Tree;
+struct tree{
+Tree left,right;
+int element;
+} 
+Tree T=NULL;
+
+Tree insert(int x,Tree T){
+    if(T==NULL){
+        T=(Tree)malloc(sizeof(Tree));
+        T->element=x;
+        T->left=NULL;
+        T->right=NULL;
+    }
+    else if(x<T->element)
+        T->left=insert(x,T->left);
+    else if(x>T->element)
+        T->right=insert(x,T->right);
+    return T;
+}
+void main(){
+int el;
+printf("\n Enter the element");
+                 scanf("%d",&el);
+                 T=insert(el,T);
+} 
