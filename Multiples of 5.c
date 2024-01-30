0387cf33503e8abfaf868b5f1fb8e3baf6eb841d
Sinduja B <127333575+Sindu9156@users.noreply.github.com>
2024-01-30 20:00:24 +0530

    Multiples of 5.c

---
+++ Multiples of 5.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main(){
+int n;
+printf("enter the value") ;
+scanf("%d", &n);
+if(n%5==0)
+     printf("the number is multiples of 5");
+else
+     printf("the number is not a multiples of 5 ");
+} 
