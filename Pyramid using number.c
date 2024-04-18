7b4cde50c68e129f1cf1db4d6ae8ad0b56a1aee2
Sinduja B <127333575+Sindu9156@users.noreply.github.com>
2024-04-18 20:35:46 +0530

    Pyramid using number.c

---
+++ Pyramid using number.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+ int n, c, d, num = 1, space;
+ scanf("%d",&n);
+ space = n - 1;
+ for ( d = 1 ; d <= n ; d++ )
+ {{
+ printf("%d", n);
+ n++;
+ }
+ num--;
+ num--;
+ for ( c = 1 ; c < d ; c++)
+ {
+ printf("%d", num);
+ num--;
+ }
+ printf("\n");
+ }
+ 
