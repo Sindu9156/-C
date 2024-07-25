de94fae4a967942eaf54a2e268859ce6ed015eed
Sinduja B <127333575+Sindu9156@users.noreply.github.com>
2024-07-25 22:18:17 +0530

    Bitwise or operator.c

---
+++ Bitwise or operator.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void main() {
+  
+  int n, n1;
+
+  printf("Enter a number: ");
+  scanf("%d", & n);
+printf("Enter a number: ");
+  scanf("%d", & n1);
+
+    printf(n|n1);
+
+}
